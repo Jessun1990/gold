b4a77430552463311cea760c34cf4aada0cf18ad
jessun <jessun1990@gmail.com>
2020-03-06 01:24:47 +0800

    Add: jsoniter

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jessun2017/gold
 
 go 1.14
+
+require github.com/json-iterator/go v1.1.9
---
+++ utils/json.go
@@ -0,0 +1,7 @@
+package utils
+
+import (
+	jsoniter "github.com/json-iterator/go"
+)
+
+var JSONITER = jsoniter.ConfigCompatibleWithStandardLibrary
