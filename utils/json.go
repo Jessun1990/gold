3e9d268e5440ad7eb99bef270bfe755883de3b09
jessun <jessun1990@gmail.com>
2020-03-15 21:49:24 +0800

    完善注释

--- utils/json.go
+++ utils/json.go
@@ -1,5 +1,17 @@
 package utils
 
+/*
+对 json-iterator 的二次封装
+*****************
+import "github.com/jessun2017/gold/utils"
+...
+var jsoniter utils.JSONITER
+jsoniter.Marshal(*, *)
+
+*****************
+
+*/
+
 import (
 	jsoniter "github.com/json-iterator/go"
 )
